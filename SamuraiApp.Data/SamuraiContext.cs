b20f21981f3bb8a0984aa7eb4c1875de6832d80d
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-21 10:51:25 +0100

    Add EF package Create class contex with dbset

---
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    class SamuraiContext : DbContext
+    {
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+    }
+}
