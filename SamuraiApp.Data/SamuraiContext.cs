0a67c32c6961edfceb583e06aba424e130080c20
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-21 12:45:59 +0100

    Add EF tools

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -3,7 +3,7 @@ using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-    class SamuraiContext : DbContext
+    public class SamuraiContext : DbContext
     {
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
