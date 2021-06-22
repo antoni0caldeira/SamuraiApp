b4c2972782ea0695f4c4a21e951dec031e61d67f
antoniocaldeira@yahoo.com <antoniocaldeira@yahoo.com>
2021-06-22 11:39:56 +0100

    Filtering queries

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 
@@ -11,28 +12,55 @@ namespace SamuraiApp.UI
 
         private static void Main(string[] args)
         {
-            _context.Database.EnsureCreated();
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
+
+            //AddSamuraisByName("Julie", "Sampson", "Julie", "Sampson");
+            //AddVariusTypes();
+            GetSamurais();
             Console.Write("Press any key...");
             Console.ReadKey();
         }
 
-        private static void AddSamurai()
+        private static void AddVariusTypes()
         {
-            Samurai samurai = new() { Name = "Asamue" };
-            _context.Samurais.Add(samurai);
+            _context.AddRange(
+                new Samurai { Name = "Shimada" },
+                new Samurai { Name = "Okamoto" },
+                new Battle { Name = "Battle of Anegawa" },
+                new Battle { Name = "Battle of Nagashimo" });
+            _context.Samurais.AddRange(
+                new Samurai { Name = "Shimada" },
+                new Samurai { Name = "Okamoto" }
+                );
+            _context.Battles.AddRange(
+                new Battle { Name = "Battle of Anegawa" },
+                new Battle { Name = "Battle of Nagashimo" }
+                );
+            _context.SaveChanges();
+
+        }
+
+        private static void AddSamuraisByName(params string[] names)
+        {
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+
+            }
             _context.SaveChanges();
         }
-        private static void GetSamurais(string text)
+        private static void GetSamurais()
         {
-            var samurais = _context.Samurais.ToList();
-            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
-            foreach (var samurai in samurais)
+            var query = _context.Samurais
+                .Where(x => x.Name.Contains("Julie") || EF.Functions.Like(x.Name, "%Shimada%"))
+                .TagWith("From get")
+                .ToList()
+                .OrderBy(n => n.Name);
+            foreach (var item in query)
             {
-                Console.WriteLine(samurai.Name);
+                Console.WriteLine(item.Name);
             }
+            Console.WriteLine($" Samurai count is {query.Count()}");
+
         }
     }
 }
